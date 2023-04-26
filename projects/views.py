22311ac9ee70994c17bb230a0f2dda8eca147235
Pete <pete.zink03@gmail.com>
2023-04-26 10:35:17 -0400

    Final submission

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
 from projects.forms import ProjectForm
